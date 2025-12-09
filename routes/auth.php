c4cb1244653a35a6d93a93c33876957a9c58c562
keith <keithzano@gmail.com>
2025-12-09 17:43:04 +0200

    change confirm password route

--- routes/auth.php
+++ routes/auth.php
@@ -48,9 +48,11 @@ Route::middleware('auth')->group(function () {
         ->name('verify-email.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+        ->name('confirm-password.show');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store'])
+        ->name('confirm-password.store');
+
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
